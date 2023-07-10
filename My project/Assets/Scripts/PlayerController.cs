42ac7d6b0891731485e08b085fc2975d47b20e3a
AhmetFurkanUtsal <furkan.utsal@hacettepe.edu.tr>
2023-07-10 16:59:23 +0300

    v2

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private float speed = 20.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float verticalInput;
     void Start()
     {
         
@@ -15,6 +20,11 @@ public class PlayerController : MonoBehaviour
     {
         // transform.Translate(0,0,1);
 
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
